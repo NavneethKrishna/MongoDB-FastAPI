84257a6d5707ca8fc60fe03061cf74b6c86ee6a1
NavneethKrishna <navneethkrishna2001@gmail.com>
2022-01-13 14:02:11 +0530

    Update MongoDB.py

--- MongoDB.py
+++ MongoDB.py
@@ -1,3 +1,8 @@
 from pymongo import MongoClient
 
-CONNECTION_STRING = "mongodb+srv://Sandbox:<password>@sandbox.hroxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://<username>:<password>@cluster0.hroxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+client = MongoClient(CONNECTION_STRING)
+db = client['trips']
+
+print(list(db.sample_training.find({"birth year" : 1969})) )
