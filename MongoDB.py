50f74bb05813113b1e927e7aa16d49577517ed78
NavneethKrishna <navneethkrishna2001@gmail.com>
2022-01-14 12:23:33 +0530

    update

--- MongoDB.py
+++ MongoDB.py
@@ -1,5 +1,6 @@
-import collections
+from pprint import pprint
 from pymongo import MongoClient
+from datetime import datetime
 
 CONNECTION_STRING = "mongodb+srv://m001-student:student@cluster0.hroxy.mongodb.net/sample_training?retryWrites=true&w=majority"
 
@@ -7,4 +8,26 @@ client = MongoClient(CONNECTION_STRING)
 db = client['sample_training']
 collection = db['trips']
 
-print(len(list(collection.find({ }, {"start station id": 1, "_id":False}).distinct())))
+"""
+insert_records = {"tripduration" : 130,
+                   "start station id": "SBC",
+                   "start station name": "Bengaluru City Junction",
+                   "end station code": "MYS",
+                   "end station name": "Mysore City Junction",
+                   "bikeid": 18007,
+                   "usertype": "Subscriber",
+                   "birth year": 1994,
+                   "start station location": {"type": "Point", 
+                                              "coordinates":[12.9781, 77.5695]
+                                              },
+                   "end station location": {"type": "Point", 
+                                              "coordinates":[12.3163, 76.6454]
+                                              },
+                    "start time":datetime(2021, 5, 28, 11, 00, 00, 0000),
+                    "stop time":datetime(2021, 5, 28, 1, 00, 00, 0000)
+                    }
+
+collection.insert_one(insert_records)
+collection.insert_many(insert_records)
+"""
+
