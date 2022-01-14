0980d4af7a47048576c5e98a047ac3ffd111a85c
NavneethKrishna <navneethkrishna2001@gmail.com>
2022-01-14 08:59:01 +0530

    Update MongoDB.py

--- MongoDB.py
+++ MongoDB.py
@@ -1,8 +1,10 @@
+import collections
 from pymongo import MongoClient
 
-CONNECTION_STRING = "mongodb+srv://<username>:<password>@cluster0.hroxy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+CONNECTION_STRING = "mongodb+srv://m001-student:student@cluster0.hroxy.mongodb.net/sample_training?retryWrites=true&w=majority"
 
 client = MongoClient(CONNECTION_STRING)
-db = client['trips']
+db = client['sample_training']
+collection = db['trips']
 
-print(list(db.sample_training.find({"birth year" : 1969})) )
+print(len(list(collection.find({ }, {"start station id": 1, "_id":False}).distinct())))
